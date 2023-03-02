8c701e159faccb7f4addea051973a7744ad0a041
SantiA25 <80929184+SantiA25@users.noreply.github.com>
2023-03-02 20:51:13 -0300

    commiteo

--- recuperatorio.pl
+++ recuperatorio.pl
@@ -1,10 +1,9 @@
-
  %      (Programa,Rating)
 programa(intrusos, 10).
 programa(infama, 11).
 programa(animalesSueltos, 8).
 programa(elMonologo, 5).
-
+programa(espn, 100).
 %      (Programa,Conductor)
 conduce(intrusos, rial).
 conduce(intrusos, ventura).
@@ -13,16 +12,26 @@ conduce(animalesSueltos, fantino).
 conduce(animalesSueltos, neumann).
 conduce(animalesSueltos, cocoSily).
 conduce(elMonologo, cocoSily).
+conduce(intrusos,ruben).
+conduce(espn,santi).
+conduce(intruso,santi).
+conduce(infama,santi).
 
-%        (Protagonista,Valor,Programa)
+%        (Protagonista,Publico,Programa)
 primicia(rial, 20, intrusos).
 primicia(fantino, 40, intrusos).
 primicia(floppyTesouro, 100, animalesSueltos).
 primicia(zairaNara, 20, animalesSueltos).
+primicia(ruben, 30, espn).
 
 
 %Punto 1 
-% ranking(Primicia):-primicia(Primicia,Valor,Programa),
+%Calcular el ranking de una primicia, 
+%que son los puntos de rating que espera hacer un programa más la mitad del publico interesado de la primicia. 
+ranking(Protagonista,Valor):-
+    primicia(Protagonista,Publico,Programa),
+    programa(Programa,Rating),
+    Valor is Rating + (Publico /2).
 
 %Punto 2
 %Se quiere conocer los programas que no tienen primicias
@@ -33,31 +42,19 @@ noTienePrimicia(Programa):-
 
 %Punto 3
 % Se quiere conocer los conductores que estén en más de un programa de chimentos.
-conductoresconMasprog(Conductor):-
-    %   programa(P1,_),
-    %   programa(P2,_),    
-    %   conduce(P1,Conductor),
-    %   conduce(P2,Conductor),
-    %   P1 \= P2.
-    conduce(_,Conductor),
-    forall(conduce(Programa,Conductor),conduce(Programa,Conductor)).
-
-
-
 masDeUnPrograma(Conductor):-
-        programa(P1,_),
-        programa(P2,_),
         conduce(P1,Conductor),
         conduce(P2,Conductor),
         P1 \= P2.
 
+
 %Punto 4
 % Se quiere saber los programas donde todas sus primicias son de conductores de otros programas de chimentos. 
 
-primiciaConConductorChimento(Programa):-
-    conduce(Programa,C),
-    primicia(Conductor,_,Programa), 
-    conduce(Conductor,Programa1),
-    C\=Conductor,
-    Programa\= Programa1. 
+conPrimiciasDeOtroConductor(Programa):-
+        programa(Programa,_),
+        primicia(_,_,Programa),
+        forall((primicia(Protagonista,_,Programa),
+        (conduce(Programa,Conductor))),
+        Protagonista \= Conductor).
 
